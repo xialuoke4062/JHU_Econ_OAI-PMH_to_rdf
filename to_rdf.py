bc8773de235a735029dc2b309122cdb48ee50500
Stanley <apple@Apples-MacBook-Pro.local>
2021-08-16 14:52:04 -0400

    change record from oai_dc to dim, and its corresponding formatting. Added fields for JEL and Creation/Revision Date since now they are available

--- to_rdf.py
+++ to_rdf.py
@@ -2,13 +2,16 @@
 from sickle import Sickle
 from pathlib import Path
 from sys import argv
+from bs4 import BeautifulSoup
 import os
 import time
 
-def add_entry(key, data_dict, file, field):
-    if key in data_dict:
-        for elem in data_dict[key]:
-            file.write(field + elem + '\n')
+def add_entry(metatags, file, field):
+    for taglia in metatags:
+        elem = taglia.text
+        if elem=="":
+            return
+        file.write(field + elem + '\n')
 
 def to_rdf(argv):
     var = "append"
@@ -23,15 +26,17 @@ def to_rdf(argv):
     ### col_1774.2_64315 is Economics, Department of / Economics Working Paper Archive
     ### col_1774.2_34121 is Biomedical Engineering, Dept. of / Economic Health Care Technologies Design
     URL = 'https://jscholarship.library.jhu.edu/oai/request?set=col_1774.2_64315'
-    URL = 'https://jscholarship.library.jhu.edu/oai/request?set=col_1774.2_34121'
-    URL = 'https://jscholarship.library.jhu.edu/oai/request?set=col_1774.2_40418'
+    # URL = 'https://jscholarship.library.jhu.edu/oai/request?set=col_1774.2_34121'
+    # URL = 'https://jscholarship.library.jhu.edu/oai/request?set=col_1774.2_40418'
     dst_file = '/Users/apple/Dropbox/JHU_Econ_OAI-PMH_to_rdf/result.rdf'   ## In case cron job needs absolute address
+    # dst_file = '//shiner.win.ad.jhu.edu/repec/result_20210813_raw.rdf' # Connect to VPN. map drive "\\shine.win.ad.jhu.edu\repec" on Windows invert backlash for Mac. 
     # dst_file = './result.rdf'
-    dst_titles = []
+    dst_ids = []
 
     ### Retrieving all records from specific community or collection
     sickle = Sickle(URL)
-    records = sickle.ListRecords(metadataPrefix='oai_dc')
+    # records = sickle.ListRecords(metadataPrefix='oai_dc')
+    records = sickle.ListRecords(metadataPrefix='dim')
 
     ### Creating brand new rdf file, overriding previous file if already exists
     if var == "override" or not Path(dst_file).exists():
@@ -40,8 +45,8 @@ def to_rdf(argv):
     elif var == "append":
         read_file = open(dst_file, 'r')
         for line in read_file:
-            if line[:7] == "Title: ":
-                dst_titles.append(line[7:].rstrip())
+            if line[:25] == "Handle: RePEc:jhu:papers:":
+                dst_ids.append(line[25:].rstrip())
         read_file.close()
         file = open(dst_file, 'a')
     else:
@@ -49,58 +54,77 @@ def to_rdf(argv):
 
     ### translating each paper entry into rdf entries
     for record in records:
-        data_dict = record.metadata
-        ### Remove all None values
-        for key, value in data_dict.items():
-            data_dict[key] = [x for x in value if x is not None]
-        print(data_dict)
+        soupy = BeautifulSoup(record.raw, 'lxml')
+
+        ### Get current record's ID
+        metatags = soupy.find_all(attrs={'element':'identifier', 'qualifier':'uri'})
+        for taglia in metatags:
+            if taglia.text=="":
+                break
+            id = taglia.text[-5:]
 
         ### Skip existing articles based on title, assuming there will
         ### never be changes to the entries
         if var == "append" and Path(dst_file).exists():
-            if data_dict['title'][0] in dst_titles:
-                print('Skipping existing article: ' + data_dict['title'][0])    # Debug purpose
+            if id in dst_ids:
+                print('Skipping existing article: ' + id)    # Debug purpose
                 continue
-        print('Recording new article: ' + data_dict['title'][0])    # Debug purpose
+        print('Recording new article: ' + id)    # Debug purpose
 
         # 1. Template-type
         file.write('Template-type: ReDIF-Paper 1.0\n')
         # 2. Author-Name
-        add_entry('creator', data_dict, file, "Author-Name: ")
+        metatags = soupy.find_all(attrs={'element':'contributor', 'qualifier':'author'})
+        add_entry(metatags, file, "Author-Name: ")
         # 3. Author-Email: not provided on JHU library
         # 4. AUthor-Homepage: not provided on JHU library
         # 5. Author-Workplace-Name: not provided on JHU library
         # 6. Author-Workplace-Homepage: not provided on JHU library
         # 7. Title
-        add_entry('title', data_dict, file, "Title: ")
+        metatags = soupy.find_all(attrs={'element':'title'})
+        add_entry(metatags, file, "Title: ")
         # 8. Abstract
-        add_entry('description', data_dict, file, "Abstract: ")
-        # 9. Classification-JELL: Invalid value 'JEL Classification' of type <jel> (attribute 'classification-jel', eval)
+        metatags = soupy.find_all(attrs={'element':'description', 'qualifier':'abstract'})
+        add_entry(metatags, file, "Abstract: ")
+        # 9. Classification-JEL
+        metatags = soupy.find_all(attrs={'element':'subject', 'qualifier':'jel'})
+        jels = []
+        for taglia in metatags:
+            elem = taglia.text
+            if elem=="": break
+            jels.append(elem)
+        if len(jels) != 0:
+            file.write("Classification-JEL: " + ", ".join(jels) + '\n')
         # 10. Keywords
-        if 'subject' in data_dict:
-            file.write("Keywords: " + ", ".join(data_dict['subject']) + '\n')
+        metatags = soupy.find_all(attrs={'element':'subject'})
+        keywords = []
+        for taglia in metatags:
+            elem = taglia.text
+            if elem in jels: continue
+            if elem=="": break
+            keywords.append(elem)
+        if len(keywords) != 0:
+            file.write("Keywords: " + ", ".join(keywords) + '\n')
         # 11. Note
         # 12. Length: pages
         # 13. Creation-Date: yyyy-mm-dd
-        if 'date' in data_dict:
-            file.write("Creation-Date: " + data_dict['date'][0][:10] + '\n')
+        metatags = soupy.find_all(attrs={'element':'date', 'qualifier':'issued'})
+        add_entry(metatags, file, "Creation-Date: ")
         # 14. Revision-Date: yyyy-mm-dd: difficult to specify
+        metatags = soupy.find_all(attrs={'element':'date', 'qualifier':'modified'})
+        add_entry(metatags, file, "Revision-Date: ")
         # 15. Number
         # 16. Publication-Status: Start value with 'Published' or 'Forthcoming'
         # 17. Price
         # 18. File-URL
-        if 'identifier' in data_dict:
-            file.write("File-URL: " + data_dict['identifier'][0] + '\n')
+        metatags = soupy.find_all(attrs={'element':'identifier', 'qualifier':'uri'})
+        add_entry(metatags, file, "File-URL: ")
         # 19. File-Format
         # 20. File-Restriction
         # 21. File-Function
         # 22. File-Size
         # 23. Handle: RePEc:aaa:ssssss: Required but don't have information
-        if 'identifier' in data_dict:
-            sample_str = str(data_dict['identifier'][0])
-            # Get last 5 character
-            hdl = sample_str[-5:]
-            file.write("Handle: RePEc:jhu:papers:" + hdl + '\n')
+        file.write("Handle: RePEc:jhu:papers:" + id + '\n')
         file.write('\n')
     file.close()
 
